27b80fff4b7bda576e4d507f59a2f874365fe827
Vladimir Popov <popov.v.aleksandrovich@sberbank.ru>
2019-07-10 14:05:28 +0300

    two ports

--- channel-add-org.sh
+++ channel-add-org.sh
@@ -6,6 +6,7 @@ exampleMsg="$0 common org2"
 IFS=
 channelName=${1:?`printUsage "$usageMsg" "$exampleMsg"`}
 newOrg=${2:?`printUsage "$usageMsg" "$exampleMsg"`}
-port=${3}
-downloadMSP ${newOrg} ${port}
-addOrgToChannel $channelName $newOrg ${port}
+port1=${3}
+port2=${4}
+downloadMSP ${newOrg} ${port1}
+addOrgToChannel $channelName $newOrg ${port2}
